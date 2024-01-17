4d034ddf8fa2c86b4e001e59e4e71358e8e3805e
Yosuke Saito <yosuke.saito1202@gmail.com>
2024-01-17 20:52:03 +0900

    delete files

--- breakout/ball.py
+++
@@ -1,7 +0,0 @@
-from object_type import ObjectType
-from physical_object import PhysicalObject
-
-
-class Ball(PhysicalObject):
-    def object_type(self) -> ObjectType:
-        return ObjectType.BALL
--- breakout/object_type.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class ObjectType(Enum):
-    BALL = 1
-    PADDLE = 2
-    BRICK = 3
